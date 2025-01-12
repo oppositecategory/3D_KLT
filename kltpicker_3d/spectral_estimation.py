Fixed power spectrum calculation + Updated eigenfunction notebook

kltpicker_3d/spectral_estimation.py
@@ -1,37 +1,24 @@
 import numpy as np 
-from numpy.fft import fftn, ifftn,fftshift,ifftshift
 
 import jax 
 import jax.numpy as jnp 
+from jax.numpy import fftn,ifftn,fftshift,ifftshift
 
 
-from utils import jitted_binarysearch,create_gaussian_window
+from utils import jitted_binarysearch,gaussian_window
 
 def estimate_isotropic_powerspectrum_tensor(tomogram, ids,max_d):
     n = tomogram.shape[0]
     if max_d >= n:
-        max_d = n -1 
+        max_d = n - 1 
 
-    r, distances, _ = estimate_isotropic_autocorrelation_vector(tomogram, ids,max_d)
-
-    # Create a 3D autocorrelation tensor
-    r3 = jnp.zeros((2*n-1,2*n-1,2*n-1),dtype=jnp.float64)
-
-    grid = jnp.arange(-max_d,max_d)
-    i,j,k = jnp.meshgrid(grid,grid,grid)
-    d = (i ** 2 + j**2 + k**2)
-
-    mask = jnp.where(d <= max_d ** 2)
-    mask,idx = jitted_binarysearch(distances, d)
-    idx = jnp.clip(idx,0,distances.shape[0]-1)
-
-    r3 = r3.at[mask].set(r[idx[mask]])
+    r3 = estimate_isotropic_autocorrelation_tensor(tomogram,  max_d,ids)
 
     # A gaussian window to truncate the Fourier transform 
-    gaussian_window = create_gaussian_window(n,max_d)
+    window = gaussian_window(max_d,n)
 
     # Estimate the 3D power spectrum by Wiener-Khinchin theorem
-    p3 = fftshift(fftn(ifftshift(r3*gaussian_window))).real
+    p3 = fftshift(fftn(ifftshift(r3*window))).real
 
     # energy is equal to the total average energy of the samples
     energy = jnp.sum(jnp.square(tomogram[ids] - np.mean(tomogram[ids])))
@@ -44,31 +31,30 @@ def estimate_isotropic_powerspectrum_tensor(tomogram, ids,max_d):
     return p3
 
 
-def estimate_isotropic_autocorrelation_vector(tomogram, max_d, ids):
+def estimate_isotropic_autocorrelation_tensor(tomogram, max_d, ids):
     n = tomogram.shape[0]
 
     grid = jnp.arange(max_d+1)
     i,j,k = jnp.meshgrid(grid,grid,grid)
-    d = i**2 + j **2 + k**2
+    d = i**2 + j**2 + k**2
     valid_dists = jnp.where(d <= max_d ** 2)
     dists = jnp.sort(jnp.unique(d[d <= max_d ** 2]))
    
     # A distance map such that i,j,k holds the index in dsquarae
     # of distance i**2 + j**2 + k**2
-    idx = jitted_binarysearch(dists,max_d)
-    dist_map = jnp.zeros(dists.shape)
+    idx = jitted_binarysearch(dists,d)
+    dist_map = jnp.zeros(d.shape, dtype=jnp.int32)
     dist_map = dist_map.at[valid_dists].set(idx[valid_dists])
 
 
-    # compute the ACF of the 1-constant signal to count number of k1,k2,k3
-    # such that k1**2 + k2 ** 2 + k3 ** 2 = d for given d
-    c = jnp.zeros(max_d) 
+    # compute the ACF of the 1-constant siganl to count number of k1,k2,k3
+    # such that k1**2 + k2 ** 2 + k3 ** 2 = d 
     mask = jnp.zeros((n,n,n))
     mask = mask.at[ids].set(1) 
     tmp = jnp.zeros((2*n+1,2*n+1,2*n+1))
-    tmp.at[:n,:n,:n].set(mask) 
+    tmp = tmp.at[:n,:n,:n].set(mask) 
     c_padded = calculate_autocorrelation(tmp)
-    c = c.set(c_padded[:max_d+1,:max_d+1,:max_d+1])
+    c = c_padded[:max_d+1,:max_d+1,:max_d+1]
     c = jnp.round(c.real).astype('int')
 
     tomogram_fft = jnp.zeros((2*n+1,2*n+1,2*n+1),dtype=jnp.complex64)
@@ -76,23 +62,30 @@ def estimate_isotropic_autocorrelation_vector(tomogram, max_d, ids):
     tomogram_acf = calculate_autocorrelation(tomogram_fft).real
 
     init = jnp.zeros((2,dists.shape[0]))
-    r,cnt = average_acf_radially(tomogram_acf, dist_map, valid_dists,c,init)
-    idx = jnp.where(cnt != 0)[0]
-    
-    r = r.at[idx].set(r[idx] / cnt[idx])
-    idx = jnp.where(r == 0)
-    dists.at[idx].set(0)
-    return r, dists, cnt
+    r,cnt = accumulate_acf_radially(tomogram_acf, dist_map, valid_dists,c,init)
+    idx1 = jnp.where(cnt != 0)[0]
+
+    r = r.at[idx1].set(r[idx1] / cnt[idx1])
+    idx1 = jnp.where(r == 0)
+    dists = dists.at[idx1].set(0)
+
+    r3 = jnp.zeros((2*n-1,2*n-1,2*n-1),dtype=jnp.float64)
+
+    outside = (valid_dists[0]+(n-1),
+               valid_dists[1]+(n-1),
+               valid_dists[2]+(n-1))
+    r3 = r3.at[outside].set(r[idx[valid_dists]])
+    return r3
 
 @jax.jit
 def calculate_autocorrelation(patch):
-    # Estiming the autocorrelation by Wiener-Khinchin theorem
+    # Estimating the autocorrelation by Wiener-Khinchin theorem
     patch_fft = fftn(patch)
     acf = ifftn(patch_fft * jnp.conj(patch_fft))
     return acf 
 
 @jax.jit
-def average_acf_radially(acf,dist_map,valid_dists,dists_counts,init):
+def accumulate_acf_radially(acf,dist_map,valid_dists,dists_counts,init):
     def scan_fn(carry, idx):
         i,j,k = idx
         d = dist_map[i,j,k]
@@ -105,8 +98,9 @@ def average_acf_radially(acf,dist_map,valid_dists,dists_counts,init):
     return v[0,...],v[1,...]
 
 
-# max_d = 10
-# X = np.random.normal(size=(10,10,10))
-# ids = np.array([[[i**2 + j ** 2 + k**2 for i in range(10)] for j in range(10)] for k in range(10)])
-# ids = np.where(ids<max_d)
-# estimate_isotropic_autocorrelation_1D(X,max_d, ids)
+max_d = 10
+key = jax.random.key(0)
+X = jax.random.normal(key,shape=(10,10,10))
+ids = jnp.array([[[i**2 + j ** 2 + k**2 for i in range(10)] for j in range(10)] for k in range(10)])
+ids = jnp.where(ids<max_d**2)
+estimate_isotropic_powerspectrum_tensor(X,ids,max_d)

kltpicker_3d/utils.py
@@ -3,17 +3,20 @@ import jax
 import numpy as np
 import jax.numpy as jnp
 
+from functools import partial
+
 @jax.jit
 def jitted_binarysearch(X,query):
     idx = jnp.searchsorted(X,query,side='left')
     return idx
 
-
-def create_gaussian_window(k,max_d):
-    l = 2*k-1 
-    xx = np.array([[[i**2 + j ** 2 + k **2 for i in range(max_d+1)] for j in range(max_d+1)] for k in range(max_d+1)])
-    xx = xx - k + 1
+@partial(jax.jit, static_argnames=['n'])
+def gaussian_window(max_d,n):
+    l = 2*n-1 
+    grid = jnp.arange(l)
+    i,j,k = jnp.meshgrid(grid,grid,grid)
+    i,j,k = i - (n-1),j-(n-1),k-(n-1)
+    xx = i**2 + j**2 + k**2
     alpha = 3.0
-    window = np.exp(-alpha * (xx ** 2)/ ( 2 * max_d ** 2))
+    window = jnp.exp(-alpha * (xx ** 2)/ ( 2 * max_d ** 2))
     return window 
-    
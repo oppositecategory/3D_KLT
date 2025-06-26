fb81827ee566a8c84f2bf5667593008eb080fd14
oppositecategory <christyaccount@hotmail.com>
2025-06-26 19:53:21 +0300

    .

--- kltpicker_3d/spectral_estimation.py
+++ kltpicker_3d/spectral_estimation.py
@@ -6,51 +6,40 @@ import jax
 import jax.numpy as jnp 
 from jax.numpy.fft import fftn,ifftn,fftshift,ifftshift
 
-from kltpicker_3d.utils import jitted_binarysearch,gaussian_window
+from kltpicker_3d.utils import jitted_binarysearch
 
-def estimate_isotropic_powerspectrum_tensor(tomograms,max_d):
+def estimate_isotropic_powerspectrum_tensor(tomogram,max_d):
     """ 
     Estimates the isotropic power spectrum of a given 3D tomogram using the autocorrelation of the tomogram.
 
     Args:
-        tomograms: 4-dimensional tensor containing samples of noisy tomograms.
+        tomogram: 3-dimensional tensor containing sample of noisy tomogram.
         max_d: the maximum distance for the isotogrpic powerspectrum
 
     Returns:
         p3: 3D Power Spectrum tensor
     """
-    K,N,_,_ = tomograms.shape
+    N,_,_ = tomogram.shape
 
     if max_d >= N:
         max_d = N - 1 
 
-    r3 = estimate_isotropic_autocorrelation(tomograms,max_d)
+    r3 = estimate_isotropic_autocorrelation(tomogram,max_d)
 
-    # A gaussian window to truncate the Fourier transform 
-    # The constatn 3 comes to "shrink" the width of the transform.
     w = jnp.array(window(('gaussian', max_d), (2*N-1,2*N-1,2*N-1)))
 
-
-    # Estimate the 3D power spectrum by Wiener-Khinchin theorem
     p3 = cfftn(r3*w).real
     p3 = jnp.where(p3 < 0, 0, p3)
 
-    # Energy is equal to the total average energy of the samples
-    E = 0
-    for k in range(K):
-        sample = tomograms[k]
-        E += jnp.sum((sample - jnp.mean(sample)) ** 2)
-
-    mean_energy = E/ (K * N ** 3)
+    mean_energy = jnp.sum(jnp.square(tomogram - jnp.mean(tomogram))) / N**3
 
     # Normalize the 3D power spectrum to preserve mean energy
     p3 = (p3/p3.sum())*mean_energy * p3.size
-    p3 = jnp.where(p3 < 0, 0, p3) # Truncate negative values by 0 
+    p3 = jnp.where(p3 < 0, 0, p3) 
     return p3
 
-
-def estimate_isotropic_autocorrelation(tomograms, max_d):
-    K,N,_,_ = tomograms.shape
+def estimate_isotropic_autocorrelation(tomogram, max_d):
+    N,_,_ = tomogram.shape
 
     grid = jnp.arange(max_d+1)
     i,j,k = jnp.meshgrid(grid,grid,grid)
@@ -72,36 +61,17 @@ def estimate_isotropic_autocorrelation(tomograms, max_d):
     c_padded = calculate_autocorrelation(tmp)
     c = c_padded[:max_d+1,:max_d+1,:max_d+1]
     c = jnp.round(c.real).astype(jnp.float32)
-
-    
-    corrs_init = jnp.zeros_like(dists,dtype=jnp.float32)
-    corrcount_init = jnp.zeros_like(dists,dtype=jnp.float32)
-
-    def volume_processing(k, carry):
-        corrs, corrcount = carry
-        sample = tomograms[k]
-
-        tomogram_fft = jnp.zeros((2*N+1, 2*N+1, 2*N+1), dtype=jnp.complex64)
-        tomogram_fft = tomogram_fft.at[:N, :N, :N].set(sample)
-
-        tomogram_acf = calculate_autocorrelation(tomogram_fft).real
-        tomogram_acf = tomogram_acf[:max_d+1, :max_d+1, :max_d+1]
-
-        init = jnp.zeros((2, dists.shape[0]))
-        r, cnt = accumulate_acf_radially(tomogram_acf, dist_map, valid_dists, c, init)
-        return (corrs + r, corrcount + cnt)
-
-    corrs, corrcount =  jax.lax.fori_loop(
-    0,K, volume_processing,(corrs_init, corrcount_init)
-    )
-    idx1 = jnp.where(corrcount != 0)[0]
-    result = corrs.at[idx1].set(corrs[idx1] / corrcount[idx1])
-
+    tomogram_fft = jnp.zeros((2*N+1,2*N+1,2*N+1),dtype=jnp.complex64)
+    tomogram_fft = tomogram_fft.at[:N,:N,:N].set(tomogram)
+    tomogram_acf = calculate_autocorrelation(tomogram_fft).real
+    init = jnp.zeros((2,dists.shape[0]))
+    r,cnt = accumulate_acf_radially(tomogram_acf, dist_map, valid_dists,c,init)
+    nonzero_mask = cnt != 0
+    result = jnp.where(nonzero_mask, r / cnt, 0.0)
     r3 = create_autocorrelation_tensor(result, dists, N, max_d)
     return r3
 
-
-@partial(jax.jit, static_argnames=['N','max_d'])
+#@partial(jax.jit, static_argnames=['N','max_d'])
 def create_autocorrelation_tensor(r, dists, N, max_d):
     r3 = jnp.zeros((2*N-1,2*N-1,2*N-1))
     for i in range(-max_d, max_d):
@@ -113,18 +83,18 @@ def create_autocorrelation_tensor(r, dists, N, max_d):
                     r3 = r3.at[(N-1) + i, (N-1) +j, (N-1)+k].set(r[id])
     return r3
 
-@jax.jit
+#@jax.jit
 def cfftn(x):
     return fftshift(fftn(ifftshift(x)))
 
-@jax.jit
+#@jax.jit
 def calculate_autocorrelation(patch):
     # Estimating the autocorrelation by Wiener-Khinchin theorem
     patch_fft = fftn(patch)
     acf = ifftn(patch_fft * jnp.conj(patch_fft))
     return acf 
 
-@jax.jit
+#@jax.jit
 def accumulate_acf_radially(acf,dist_map,valid_dists,dists_counts,init):
     """ The function transforms the autocorrelation function into it's
         isotoropic form by averaging over all possible distances of given distance.
@@ -139,11 +109,3 @@ def accumulate_acf_radially(acf,dist_map,valid_dists,dists_counts,init):
     vv = jnp.stack(valid_dists,axis=-1)
     v, _ = jax.lax.scan(scan_fn,init,vv)
     return v[0,...],v[1,...]
-
-
-# max_d = 10
-# key = jax.random.key(0)
-# X = jax.random.normal(key,shape=(10,10,10))
-# ids = jnp.array([[[i**2 + j ** 2 + k**2 for i in range(10)] for j in range(10)] for k in range(10)])
-# ids = jnp.where(ids<max_d**2)
-# print(estimate_isotropic_powerspectrum_tensor(X,ids,max_d))
--- kltpicker_3d/utils.py
+++ kltpicker_3d/utils.py
@@ -5,6 +5,33 @@ import jax.numpy as jnp
 
 from functools import partial
 
+def trigonometric_interpolation(x,y,z):
+    """ 
+    Vectorized trigonometric interpolation for equidistant points. 
+
+    args:
+        - x: equidistant sample points 
+        - y: function values at sampled points
+        - z: evaluation points for interpolation
+    """
+    n = x.shape[0]
+    
+    scale = (x[1] - x[0]) * n / 2 
+    x_scaled = (x / scale) * np.pi / 2 
+    z_scaled = (z / scale) * np.pi / 2
+
+    delta = z_scaled[:, None] - x_scaled[None, :]
+
+    if n % 2 == 0:
+        M = np.sin(n*delta) / (n *np.sin(delta))
+    else:
+        M = np.sin(n*delta)/ (n*np.tan(delta))
+    M[np.isclose(delta,0)] = 1
+
+    p = M @ y 
+    return p
+
+
 @jax.jit
 def jitted_binarysearch(X,query):
     idx = jnp.searchsorted(X,query,side='left')

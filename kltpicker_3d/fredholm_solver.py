742c58f95fff8c3e7ad8646b22cae7b3cd06434c
oppositecategory <christyaccount@hotmail.com>
2025-10-29 11:55:55 +0200

    updated Fredholm calculation + added template creation

--- kltpicker_3d/fredholm_solver.py
+++ kltpicker_3d/fredholm_solver.py
@@ -1,108 +1,131 @@
-import numpy as np 
+import numpy as np
 import scipy
+from scipy.special import spherical_jn, sph_harm
 
-import jax 
-import jax.numpy as jnp
-from jax.numpy import einsum
+def solve_radial_fredholm_equation(Gx: np.ndarry,
+                           N: int,
+                           a: float,
+                           c: float,
+                           K=150):
+    """
+        Solves radial KLT integral equation using Nystrom method.
+        
+        args:
+            Gx: particle function's radial PSD
+            a: particle diameter
+            c: particle function's bandlimit 
+            K: Legendre quadrature order (bounds the n indices in the solutions)
 
-from scipy.special import legendre
-import scipy.integrate as integrate
+        returns:
+            eigvals: eigenvalues lambda_{N,n} for n below K
+            eigfuncs: eigenfunctions R_{N,n} for n below K
+    """
+    def Hn(x):
+        return 2*np.pi * (2*(1j**N) * spherical_jn(N,x))
 
-from functools import partial
+    X,w = scipy.special.roots_legendre(K)
+    inner_rho = (c/2)*X + c/2
+    outer_rho = (a/2)*X + a/2
 
-def gpu_integral_equation_solver(G,a,c,N,K=150):
-   """ Solves the Kosambi–Karhunen–Loève Fredholm integral equation up to order N in parallel.
+    inner_grid = np.outer(inner_rho,inner_rho)
+    vv = Hn(inner_grid)
+    sgn = -1.0 if (N % 2) else 1.0
+    D = (c/2) * w * Gx * (inner_rho**2)
+    H = (vv * D[None,:]) @ (sgn *vv).T
+    W = a/2 * np.diag(w*(outer_rho)**2)
 
-       Args:
-        G: radial power spectrum
-        a: particle's diameter 
-        c: particle energy function's bandwidth 
-        N: the maximum order of the equation we solve for 
+    # Symmetrize the matrix
+    A = np.sqrt(W) @ H @ np.sqrt(W)    
+    eigvals, Y = np.linalg.eigh(A)
+    R = np.linalg.solve(np.sqrt(W), Y) 
 
-      Returns:
-        eigenvalues: eigenvalues of the discretized integral equation up to order N 
-        eigenfunctions: the solutions of the integral equation
-   """
-   legendre_roots, w = scipy.special.roots_legendre(K)
-   legendre_roots,w = jnp.array(legendre_roots), jnp.array(w)
+    eigvals = eigvals[::-1]
+    eigfuncs = R[:,::-1]
 
-   rho = c/2 * legendre_roots + c/2
-   r = a/2 * legendre_roots + a/2
+    eigvals = np.where(np.abs(eigvals) < np.spacing(1),0, eigvals)
+    eigfuncs[:,eigvals ==0] = 0
+    return eigvals, eigfuncs, W
 
-   # NxK matrix containing Legendre polynomial over [-1,1] 
-   # for orders up to N
-   p = jnp.array([legendre(k)(legendre_roots) for k in range(N)])
-   g_tensor = G(rho)
+def create_GPSF_templates(eigfuncs: np.ndarray, 
+                          eigvals: np.ndarray, 
+                          orders: np.ndarry,
+                          G: np.ndaary,
+                          a: float,c: float, 
+                          N: int, K=150):
+    """
+        Generates 3D templates in Generalized Prolate Spherodial Function basis
+        using radial solutions of KLT equations and their spectrum.
 
-   # Multiples of inner and outer integral varaibles
-   legendre_multiples = r[...,None]*rho[None,...]
-   
-   even_orders = jnp.arange(2,N,2)
-   odd_orders = jnp.arange(1,N,2)
-   
-   # Broadcasting the input of Hn (scaled Legendre roots multiplied) over the interval [-1,1]
-   special_fn_input = einsum("ij,k->ijk",legendre_multiples,legendre_roots)
+        args:
+            eigfuncs: radial solutions of KLT equations
+            eigvals: correspoonding eigenvalues
+            orders: the N orders of the corresponding solutions
+            G: particle function's radial PSD 
+            a: particle diameter
+            c: particle's function bandlimit
+            N: size of patch
+            K: Legendre quadrature order
+    """
+    grid = np.arange(-(N-1),N)
+    X,Y,Z = np.meshgrid(grid,grid,grid)
+    r_tensor = np.sqrt(X**2 + Y**2 + Z**2)
+    rho_uniform, idx = np.unique(r_tensor.flatten(),return_inverse=True)
 
-   p_even = p[even_orders]
-   p_odd = p[odd_orders]
+    # Legendre roots for both integrals
+    rho_leg, w = scipy.special.roots_legendre(K)
+    rho_leg_a =  (a * 0.5) * rho_leg + a / 2.0  
+    rho_leg_c =  (c * 0.5)* rho_leg + c / 2
+    
+    # Truncates the spectra at 99%
+    eigval_cumsum = np.cumsum(eigvals / np.sum(eigvals))
+    truncate_idx = (eigval_cumsum < 0.99).argmax() + 1
 
-   # Tensors of size N//2 x K x K 
-   hn_even = batched_Hn_even(special_fn_input,w,p_even) 
-   hn_odd = batched_Hn_odd(special_fn_input,w,p_odd)
+    eigfuncs = eigfuncs[:truncate_idx,...]
+    eigvals = eigvals[:truncate_idx,...]
+    orders = orders[:truncate_idx,...]
 
-   hn = jnp.zeros((N,K,K))
-   hn = hn.at[even_orders].set(hn_even)
-   hn = hn.at[odd_orders].set(hn_odd)
+    # We interpolate the radial solutions into uniform radial basis
+    # using the Fredholm equation (re-expressing new values of R_{N,m} using  
+    # values of it at Legendre roots.
+    r_grid_uni = np.outer(rho_uniform, rho_leg_c)
+    r_grid_leg = np.outer(rho_leg_a, rho_leg_c)
 
-   vv = einsum("bij,bjl->bil", hn, jnp.matrix_transpose(hn))
-   jacobian = einsum("i,j->ij",r,rho) ** 2
+    def Hn_scipy(x,N):
+        return 4*np.pi * ((1j**N) * spherical_jn(N,x))
 
-   batched_psi_matrix = einsum("k,bij,k,ik->bij",w,vv,g_tensor,jacobian)
-   
-   # As of now there's no GPU-supported eigendecomposition
-   # However as we are dealing with relatively small matrices the CPU
-   # version is quite fast + the compilation help reduces time 
-   # signifincatly for calls after the first one
-   cpu_device = jax.devices('cpu')[0]
-   cpu_psi = jax.device_put(batched_psi_matrix,device=cpu_device)
-   eigenvalues, eigenfunctions = batched_eigdecomposition(cpu_psi,N)
-   return eigenvalues, eigenfunctions
+    # Hn evaluated at multiples of uniform radial points in [0,a]
+    Hn_uniform = np.array(
+        [Hn_scipy(r_grid_uni,N) for N in range(orders.max()+1)]
+    )
 
-@jax.jit
-def batched_Hn_even(x,w,p): 
-  return 4 * jnp.pi * einsum("ijk,bk,k->bij", jnp.cos(x),p,w)
-   
-@jax.jit
-def batched_Hn_odd(x,w,p):
-  return 4* jnp.pi * einsum("ijk,bk,k->bij", jnp.sin(x),p,w)
+    # Hn evaluated at multiples Legendre roots in [0,a]
+    Hn_leg = np.array(
+        [Hn_scipy(r_grid_leg,N) for N in range(orders.max()+1)]
+    )
 
-@partial(jax.jit,static_argnames=['N'])
-def batched_eigdecomposition(X,N):
-    eigenvalues, eigenfunctions = [],[]
-    for i in range(N):
-        lambdas, eig_fns = jnp.linalg.eig(X[i,...])
-        eigenvalues.append(lambdas)
-        eigenfunctions.append(eig_fns)
-    return eigenvalues,eigenfunctions
+    Hn_leg = Hn_leg[orders]
+    Hn_uniform = Hn_uniform[orders]
 
-def cpu_integral_equation_solver(Gx,N,K=150):
-  # Slow-CPU version for testing purposes only. 
-  # NOTE: It assumes a=c=1
-  def Hn(x):
-    p = legendre(N)
-    if N % 2 == 0:
-        return 4 * np.pi * integrate.quad(lambda u: np.cos(x*u) * p(u), 0,1)[0]
-    return 4 * np.pi * integrate.quad(lambda u: np.sin(x*u),0,1)[0]
-  
-  X,w = scipy.special.roots_legendre(K)
-  X_scaled = 0.5*X + 0.5
+    sgn = np.where(orders % 2 == 1, -1, 1)
+    D = c * 0.5* w * G * (rho_leg_c**2)
+    W = a * 0.5 * np.diag(w* rho_leg_a**2)
 
-  vv = np.array([[Hn(X_scaled[i]*X_scaled[j]) for j in range(K)] for i in range(K)])
+    H_right = np.swapaxes(sgn[:,None,None]* Hn_leg, -1,-2)
+    psi = (Hn_uniform * D[None,None,:]) @ H_right
 
-  def psi(i,j):
-    return 0.5 * np.sum(w * vv[j,:] * vv[i,:] * Gx * (X_scaled[j]*X_scaled)**2)
+    # contains eigenfunctions evaluated at uniform radial sampling points
+    eigfuncs_uniform = (psi @ W @ eigfuncs.T) / eigvals
+    # TODO: Change to 3D using idx such that it contains corresponding
+    # radii
 
-  U = np.array([[w[j]*psi(i,j) for j in range(K)] for i in range(K)])
-
-  eigenvalues, eigenvectors = scipy.linalg.eig(U)
-  return eigenvalues,eigenvectors
+    # creates Spherical Harmoonics templates
+    theta = np.arctan2(Y, X)
+    phi = np.arctan2(Z,np.sqrt(X**2 + Y**2))
+    
+    #TODO: Figure out how to vecotirze sph_harm. 
+    # maybe Jax implementation can help.
+    # you only need to calculate up to highest relevant order.
+    # and for each such N calculate for |m| < N
+    sph_harm = sph_harm(np.arange(orders.max()+1),
+                        np.arange(20),
+                        theta,phi)

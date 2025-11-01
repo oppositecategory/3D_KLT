a4b6cd489021e71abdf39bec2a6954f556a9e902
oppositecategory <christyaccount@hotmail.com>
2025-11-01 15:09:34 +0200

    .

--- kltpicker_3d/fredholm_solver.py
+++ kltpicker_3d/fredholm_solver.py
@@ -2,7 +2,8 @@ import numpy as np
 import scipy
 from scipy.special import spherical_jn, sph_harm
 
-def solve_radial_fredholm_equation(Gx: np.ndarry,
+
+def solve_radial_fredholm_equation(Gx,
                            N: int,
                            a: float,
                            c: float,
@@ -46,10 +47,11 @@ def solve_radial_fredholm_equation(Gx: np.ndarry,
     eigfuncs[:,eigvals ==0] = 0
     return eigvals, eigfuncs, W
 
-def create_GPSF_templates(eigfuncs: np.ndarray, 
-                          eigvals: np.ndarray, 
-                          orders: np.ndarry,
-                          G: np.ndaary,
+
+def create_GPSF_templates(eigfuncs, 
+                          eigvals, 
+                          orders,
+                          G,
                           a: float,c: float, 
                           N: int, K=150):
     """
@@ -65,6 +67,17 @@ def create_GPSF_templates(eigfuncs: np.ndarray,
             c: particle's function bandlimit
             N: size of patch
             K: Legendre quadrature order
+        
+        returns:
+            templates: 3D templates composed of radial and angular basis functions. 
+            Shape of returned tensor: truncate_idx X max_N x (grid_shape)
+            eigvals: spectrum of all eigenfunctions truncated at 99%
+            orders: corresonding N order of each solution
+
+
+        TODO: 
+            - Clarify scale of a and how to determine patch_size. 
+            - In original code patch_size_func/2 - 1 was used. Why?
     """
     grid = np.arange(-(N-1),N)
     X,Y,Z = np.meshgrid(grid,grid,grid)
@@ -84,23 +97,25 @@ def create_GPSF_templates(eigfuncs: np.ndarray,
     eigvals = eigvals[:truncate_idx,...]
     orders = orders[:truncate_idx,...]
 
+    max_N = orders.max() + 1
+
     # We interpolate the radial solutions into uniform radial basis
     # using the Fredholm equation (re-expressing new values of R_{N,m} using  
     # values of it at Legendre roots.
     r_grid_uni = np.outer(rho_uniform, rho_leg_c)
     r_grid_leg = np.outer(rho_leg_a, rho_leg_c)
 
-    def Hn_scipy(x,N):
+    def Hn(x,N):
         return 4*np.pi * ((1j**N) * spherical_jn(N,x))
 
     # Hn evaluated at multiples of uniform radial points in [0,a]
     Hn_uniform = np.array(
-        [Hn_scipy(r_grid_uni,N) for N in range(orders.max()+1)]
+        [Hn(r_grid_uni,N) for N in range(max_N)]
     )
 
     # Hn evaluated at multiples Legendre roots in [0,a]
     Hn_leg = np.array(
-        [Hn_scipy(r_grid_leg,N) for N in range(orders.max()+1)]
+        [Hn(r_grid_leg,N) for N in range(max_N)]
     )
 
     Hn_leg = Hn_leg[orders]
@@ -108,24 +123,29 @@ def create_GPSF_templates(eigfuncs: np.ndarray,
 
     sgn = np.where(orders % 2 == 1, -1, 1)
     D = c * 0.5* w * G * (rho_leg_c**2)
-    W = a * 0.5 * np.diag(w* rho_leg_a**2)
+    W = a * 0.5 * w* rho_leg_a**2
 
-    H_right = np.swapaxes(sgn[:,None,None]* Hn_leg, -1,-2)
+    H_right = sgn[:,None,None]* Hn_leg
     psi = (Hn_uniform * D[None,None,:]) @ H_right
 
-    # contains eigenfunctions evaluated at uniform radial sampling points
-    eigfuncs_uniform = (psi @ W @ eigfuncs.T) / eigvals
-    # TODO: Change to 3D using idx such that it contains corresponding
-    # radii
+    # Shape: truncate_idx X rho_uniform.length 
+    eigfuncs_uniform = np.einsum('bik,k,bk->bi', psi,W,eigfuncs) / eigvals[:,None]
+    radial_templates = eigfuncs_uniform[:,idx]
 
-    # creates Spherical Harmoonics templates
+    # Spherical Harmonics 
     theta = np.arctan2(Y, X)
     phi = np.arctan2(Z,np.sqrt(X**2 + Y**2))
+
+    sph_harm = np.zeros((max_N,max_N) + theta.shape, dtype=np.complex64)
+    for N in range(max_N):
+        for m in range(N):
+            sph_harm[N,m] = sph_harm(m,N,theta,phi)
     
-    #TODO: Figure out how to vecotirze sph_harm. 
-    # maybe Jax implementation can help.
-    # you only need to calculate up to highest relevant order.
-    # and for each such N calculate for |m| < N
-    sph_harm = sph_harm(np.arange(orders.max()+1),
-                        np.arange(20),
-                        theta,phi)
+    templates = sph_harm[orders]*radial_templates[:,None,...]
+    return templates, eigvals, orders
+
+    
+
+
+
+
